5d4d114a8f47274c2c8c61e8ef0478b7cbd90bd0
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-10-08 11:24:47 +0700

    Произведены начальные настройки проекта

---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    'declaration-block-no-duplicate-properties': true,
+    'font-family-no-duplicate-names': true,
+    'no-duplicate-selectors': true,
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/ts/index.ts'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.css', '.scss'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin([{ filename: 'style.css' }]),
+    new StylelintPlugin({
+      configFile: path.resolve(__dirname, './stylelint.config.js'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              postcssOptions: {
+                config: path.resolve(__dirname, './postcss.config.js'),
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              postcssOptions: {
+                config: path.resolve(__dirname, './postcss.config.js'),
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
