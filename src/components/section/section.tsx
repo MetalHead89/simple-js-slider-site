b148bfe077f3eac8941994f5f28eca107ecfc19d
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-10-12 15:44:45 +0700

    Добавлен вывод items в sections

--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import { IPage, ISection } from '../../ts/interfaces';
+import { ISection } from '../../ts/interfaces';
 
 const Section = (props: ISection): JSX.Element => {
-  return <section className="section"></section>;
+  const items = props.items.map((item) => {
+    if (item.type === 'header1') {
+      return <h1>{item.value}</h1>;
+    }
+    if (item.type === 'paragraph') {
+      return <p>{item.value}</p>;
+    }
+  });
+  return <section className="section">{items}</section>;
 };
 
 export default Section;
