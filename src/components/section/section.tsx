367e0266c642b0996ba2b5f750279e22b7fb8b6b
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-10-13 20:33:30 +0700

    Добавлены новые элементы в default.confg

--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -6,8 +6,21 @@ const Section = (props: ISection): JSX.Element => {
     if (item.type === 'header1') {
       return <h1>{item.value}</h1>;
     }
+
+    if (item.type === 'header2') {
+      return <h2>{item.value}</h2>;
+    }
+
     if (item.type === 'paragraph') {
       return <p>{item.value}</p>;
+    }
+
+    if (item.type === 'ul') {
+      return (
+        <ul>
+          <li>{item.value}</li>
+        </ul>
+      );
     }
   });
   return <section className="section">{items}</section>;
